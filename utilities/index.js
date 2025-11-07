b4bfcb2054845f5bfe347f9b7185892cf1f43adc
ammon <ammonsawesome@gmail.com>
2025-11-07 11:27:16 -0700

    Fixed problem for the lebal on the class dropdown

--- utilities/index.js
+++ utilities/index.js
@@ -94,8 +94,8 @@ Util.buildRegisterGrid = async function() {
 
 Util.buildClassificationList = async function (classification_id = null) {
     let data = await invModel.getClassifications()
-    let classificationList =
-      '<select name="classification_id" id="classificationList" required>'
+    let classificationList = '<label for="classification_id">Choose a class:</label><br>'
+    classificationList += '<select name="classification_id" id="classificationList" required>'
     classificationList += "<option value=''>Choose a Classification</option>"
     data.rows.forEach((row) => {
       classificationList += '<option value="' + row.classification_id + '"'
